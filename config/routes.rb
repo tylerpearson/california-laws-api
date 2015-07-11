56b4baa4111e9d92a2da288f2e798eeaea1b8b49
Tyler Pearson <ty.pearson@gmail.com>
2015-07-10 23:00:36 -0700

    Adjust routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  namespace :api, path: '/', defaults: {format: 'json'} do
+  namespace :api, defaults: {format: 'json'} do
     namespace :v1 do
       resources :codes, only: :index
       get '/sections' => 'sections#index'
